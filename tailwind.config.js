741fcff63e55e584cb345aa33ae7a16f13f1ff3d
jonnek <jonnekoivisto00@gmail.com>
2025-02-27 17:23:40 +0200

    Still missing mobile style

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        myFont: ['myFont', 'cursive'],
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        primary: '#3f455e',
+        secondary: '#d7d4d7',
+        third: '#8f2172',
+        fourth: '#c08b3f',
+        backcolor: '#020617',
+        buttonColor: '#0f172a',
+      },
+    },
+  },
+  plugins: [],
+}
