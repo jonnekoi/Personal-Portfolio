bceb4db22eac84f9f8d4e8d36d548b395f602f29
jonnek <jonnekoivisto00@gmail.com>
2025-02-28 00:05:20 +0200

    update to contact icons, plus added github icon

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,16 +15,26 @@ const Contact = () => {
           me</h3>
         <p className="max-w-110 text-white code-text m-12">You can contact me
           either on LinkedIn or email!</p>
-        <a href="https://www.linkedin.com/in/jonnekoivisto" target="_blank"
-           rel="noopener noreferrer">
-          <i className="devicon-linkedin-plain colored colored icon-large"></i>
-        </a>
-        <button
-            className="border rounded-full p-3 w-1/6 m-auto mt-10 mb-10 text-white font-myFont button"
-            onClick={handleCopyEmail}>
-          Email
-        </button>
-        {copied && <p className="text-white code-text m-7">Email copied to
+        <div className="flex w-1/4 m-auto align-middle contact-icons">
+          <a className="w-fit m-auto contact-item"
+             href="https://www.linkedin.com/in/jonnekoivisto" target="_blank"
+             rel="noopener noreferrer">
+            <i className="devicon-linkedin-plain colored colored icon-large"></i>
+          </a>
+          <svg onClick={handleCopyEmail} xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 24 24" strokeWidth={1.5} stroke="white"
+               className="size-16 cursor-pointer contact-item">
+            <path strokeLinecap="round" strokeLinejoin="round"
+                  d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"/>
+          </svg>
+          <a className="w-fit m-auto contact-item" href="https://www.github.com/jonnekoi"
+             target="_blank"
+             rel="noopener noreferrer">
+            <i className="devicon-github-original-wordmark icon-large"></i>
+          </a>
+        </div>
+        {copied && <p className="text-orange-400 code-text m-7">Email copied to
           clipboard!</p>}
       </section>
   )
