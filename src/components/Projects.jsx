9d0e6b72a3f19d67f4870e7861c4799bf8f9cad2
jonnek <jonnekoivisto00@gmail.com>
2025-03-03 01:17:47 +0200

    add photos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -37,7 +37,8 @@ const projects = [
         "  <path strokeLinecap=\"round\" strokeLinejoin=\"round\" d=\"m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125\" />\n" +
         "</svg>\n",
     githublink: "https://github.com/jonnekoi/euro2024Front",
-    servergithublink: "https://github.com/jonnekoi/euro2024"
+    servergithublink: "https://github.com/jonnekoi/euro2024",
+    photoslink: "https://github.com/jonnekoi/euro2024Front/blob/master/ui-photos/photos.md"
   },
   {
     title: "League Guesser",
