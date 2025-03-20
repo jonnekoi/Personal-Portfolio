e0fa292c5cc44764dc23b47ab68ad90e20717ed8
Jonne Koivisto <142801048+jonnekoi@users.noreply.github.com>
2025-03-21 01:19:02 +0200

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -37,8 +37,7 @@ const projects = [
         "  <path strokeLinecap=\"round\" strokeLinejoin=\"round\" d=\"m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125\" />\n" +
         "</svg>\n",
     githublink: "https://github.com/jonnekoi/euro2024Front",
-    servergithublink: "https://github.com/jonnekoi/euro2024",
-    photoslink: "https://github.com/jonnekoi/euro2024Front/blob/master/ui-photos/photos.md"
+    servergithublink: "https://github.com/jonnekoi/euro2024"
   },
   {
     title: "League Guesser",
@@ -48,8 +47,7 @@ const projects = [
         "  <path strokeLinecap=\"round\" strokeLinejoin=\"round\" d=\"m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10\" />\n" +
         "</svg>\n",
     githublink: "https://github.com/jonnekoi/fmm-front",
-    servergithublink: "https://github.com/jonnekoi/FMM",
-    photoslink: "https://github.com/jonnekoi/fmm-front/blob/master/ui-photos/photos.md"
+    servergithublink: "https://github.com/jonnekoi/FMM"
   },
   {
     title: "Personal Trainer Platform (In progress)",
@@ -59,8 +57,7 @@ const projects = [
         "  <path strokeLinecap=\"round\" strokeLinejoin=\"round\" d=\"M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z\" />\n" +
         "</svg>\n",
     githublink: "https://github.com/jonnekoi/gfit-web",
-    servergithublink: "https://github.com/jonnekoi/GFit-Server",
-    photoslink: "https://github.com/jonnekoi/gfit-web/blob/main/ui-photos/photos.md"
+    servergithublink: "https://github.com/jonnekoi/GFit-Server"
   },
   {
     title: "Personal Portfolio",
@@ -112,12 +109,6 @@ const Projects = () => {
                          className="text-orange-400 hover:text-orange-500 m-1">
                         Backend
                       </a>
-                  )}
-                  {project.photoslink && (
-                  <a href={project.photoslink} target="_blank" rel="noreferrer"
-                      className="text-orange-400 hover:text-orange-500 m-1">
-                      Photos
-                  </a>
                   )}
                 </div>
               </motion.div>
